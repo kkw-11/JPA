6c9570f37b63257eb27013b1bff72ca0299b77f1
kiwon <kkw-11@naver.com>
2025-01-15 19:49:58 +0900

    feat:to develop OrderItem method

--- src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -1,15 +1,22 @@
 package jpabook.jpashop.domain;
 
-import jakarta.persistence.*;
+import static jakarta.persistence.FetchType.LAZY;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jpabook.jpashop.domain.item.Item;
 import lombok.Getter;
 import lombok.Setter;
-import jpabook.jpashop.domain.item.Item;
-
-import static jakarta.persistence.FetchType.*;
 
 @Entity
-@Getter @Setter
+@Getter
+@Setter
 public class OrderItem {
+
     @Id
     @GeneratedValue
     @Column(name = "order_item_id")
@@ -22,4 +29,34 @@ public class OrderItem {
     @ManyToOne(fetch = LAZY)
     @JoinColumn(name = "order_id")
     private Order order;
+
+    private int orderPrice;
+    private int count;
+
+    //== 생성 메서드 ==//
+    public static OrderItem createOrderItem(Item item, int orderPrice, int count) {
+        OrderItem orderItem = new OrderItem();
+        orderItem.setItem(item);
+        orderItem.setOrderPrice(orderPrice);
+        orderItem.setCount(count);
+        
+        item.removeStock(count);
+        return orderItem;
+    }
+
+    //== 비즈니스 로직 ==//
+    public void cancel() {
+        getItem().addStock(count);
+    }
+
+    //== 조회 로직 ==//
+
+    /**
+     * 주문 상품 전체 가격 조회
+     *
+     * @return
+     */
+    public int getTotalPrice() {
+        return getOrderPrice() * getCount();
+    }
 }
