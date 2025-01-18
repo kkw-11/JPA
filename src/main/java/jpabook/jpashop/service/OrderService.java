a3707a26384ab68cc00c1e34ae428fedb12a43c0
kiwon <kkw-11@naver.com>
2025-01-18 21:32:24 +0900

    chore:to add line spacing

--- src/main/java/jpabook/jpashop/service/OrderService.java
+++ src/main/java/jpabook/jpashop/service/OrderService.java
@@ -31,10 +31,12 @@ public class OrderService {
         //엔티티 조회
         Member member = memberRepository.findOne(memberId);
         Item item = itemRepository.findOne(itemId);
+
         //배송정보 생성
         Delivery delivery = new Delivery();
         delivery.setAddress(member.getAddress());
         delivery.setStatus(DeliveryStatus.READY);
+
         //주문상품 생성
         OrderItem orderItem = OrderItem.createOrderItem(item, item.getPrice(), count);
         //주문 생성
@@ -52,6 +54,7 @@ public class OrderService {
     public void cancelOrder(Long orderId) {
         //주문 엔티티 조회
         Order order = orderRepository.findOne(orderId);
+        
         //주문 취소
         order.cancel();
     }
